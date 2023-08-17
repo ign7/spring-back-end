01e85beb73a3d2b793f2a6ec478977d0ed969057
igor <quaresmaigor@yahoo.com>
2023-08-17 11:24:45 -0300

    voltando ao que tava

--- src/main/java/com/example/control/service/DataBaseService.java
+++ src/main/java/com/example/control/service/DataBaseService.java
@@ -1,17 +1,17 @@
-package com.example.control.service;
+//package com.example.control.service;
 
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.stereotype.Service;
+//import org.springframework.beans.factory.annotation.Value;
+//import org.springframework.stereotype.Service;
 
-@Service
-public class DataBaseService {
+//@Service
+//public class DataBaseService {
 	
 	
-	@Value("${spring.datasource.url}")
-	private  String  DB_URL;
-	@Value("${spring.datasource.username}")
-	private String DB_USER; 
-	@Value("${spring.datasource.password}")
-	private String DB_PASSWORD;
+	//@Value("${spring.datasource.url}")
+	//private  String  DB_URL;
+	//@Value("${spring.datasource.username}")
+	//private String DB_USER; 
+	//@Value("${spring.datasource.password}")
+	//private String DB_PASSWORD;
 
-}
+//}
