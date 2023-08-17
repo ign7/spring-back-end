d071aa51d93c4adee509515a6e4f13274a67946c
igor <quaresmaigor@yahoo.com>
2023-08-17 11:07:00 -0300

    variavelambiente

---
+++ src/main/java/com/example/control/service/DataBaseService.java
@@ -0,0 +1,17 @@
+package com.example.control.service;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+@Service
+public class DataBaseService {
+	
+	
+	@Value("${spring.datasource.url}")
+	private  String  DB_URL;
+	@Value("${spring.datasource.username}")
+	private String DB_USER; 
+	@Value("${spring.datasource.password}")
+	private String DB_PASSWORD;
+
+}
